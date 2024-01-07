02d5497feff1bffad62a97ef90960d65748d83e9
tmxhsk99 <tmxhsk99@naver.com>
2024-01-07 18:27:28 +0900

    feat : 상세화면 추가

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,109 @@
+import '../models/poketmon.dart';
+import 'package:flutter/material.dart';
+
+final List<String> replies = [
+  "지금 메타에서는 사용하기 힘듬",
+  "이거 쓰면 무조건 이긴다",
+  "이거 쓰면 무조건 진다",
+  "돌격조끼 사용시에는 노력치 조정이 필요",
+  "후보 기술로는 기합구슬"
+];
+
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({
+    super.key,
+    required this.poketmon,
+  });
+
+  final Poketmon poketmon;
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.poketmon.name)),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.only(
+            top: 10.0,
+            left: 10.0,
+            right: 10.0,
+          ),
+          physics: const ClampingScrollPhysics(),
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Image.asset(
+                widget.poketmon.link,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(widget.poketmon.name,
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    color: Color(
+                      0xFF777777,
+                    ),
+                  ))
+            ]),
+            Row(
+              children: [
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  icon: const Icon(
+                    Icons.thumbs_up_down_outlined,
+                  ),
+                  onPressed: () {},
+                ),
+                Text(
+                  widget.poketmon.likeCount.toString(),
+                )
+              ],
+            ),
+            Text(
+              "댓글 ${widget.poketmon.replyCount}개",
+            ),
+            ...List.generate(
+              replies.length,
+              (index) => Padding(
+                padding: const EdgeInsets.only(
+                  top: 10.0,
+                ),
+                child: Row(children: [
+                  const Text(
+                    "익명",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(horizontal: 3.0)),
+                  Text(
+                    replies[index],
+                  ),
+                ]),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: Text(
+                "${widget.poketmon.created.year}년 ${widget.poketmon.created.month}월 ${widget.poketmon.created.day}일",
+                style: const TextStyle(
+                  color: Color(
+                    0xFFAAAAAA,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
+import './detail_screen.dart';
 import '../models/poketmon.dart';
 
 final List<Poketmon> poketmons = [
-  Poketmon(id: "0", name: "거북왕", title: "껍질깨기 거북왕 샘플", link: "assets/images/gbw.png", likeCount: 1920, replyCount: 6, created: DateTime.now()),
-  Poketmon(id: "1", name: "이상해꽃", title: "퀘청팟 이상해꽃 샘플", link: "assets/images/ishgg.png", likeCount: 220, replyCount: 2, created: DateTime.now()),
-  Poketmon(id: "2", name: "피카츄", title: "물리어태커 피카츄 샘플", link: "assets/images/pika.png", likeCount: 2020, replyCount: 9, created: DateTime.now()),
-  Poketmon(id: "3", name: "리자몽", title: "선파워 리자몽 샘플", link: "assets/images/rizamong.png", likeCount: 320, replyCount: 2, created: DateTime.now()),
-  Poketmon(id: "4", name: "강챙이", title: "배북 불테라 강챙이 샘플", link: "assets/images/gce.png", likeCount: 2620, replyCount: 10, created: DateTime.now()),
+  Poketmon(
+      id: "0",
+      name: "거북왕",
+      title: "껍질깨기 거북왕 샘플",
+      link: "assets/images/gbw.png",
+      likeCount: 1920,
+      replyCount: 6,
+      created: DateTime.now()),
+  Poketmon(
+      id: "1",
+      name: "이상해꽃",
+      title: "퀘청팟 이상해꽃 샘플",
+      link: "assets/images/ishgg.png",
+      likeCount: 220,
+      replyCount: 2,
+      created: DateTime.now()),
+  Poketmon(
+      id: "2",
+      name: "피카츄",
+      title: "물리어태커 피카츄 샘플",
+      link: "assets/images/pika.png",
+      likeCount: 2020,
+      replyCount: 9,
+      created: DateTime.now()),
+  Poketmon(
+      id: "3",
+      name: "리자몽",
+      title: "선파워 리자몽 샘플",
+      link: "assets/images/rizamong.png",
+      likeCount: 320,
+      replyCount: 2,
+      created: DateTime.now()),
+  Poketmon(
+      id: "4",
+      name: "강챙이",
+      title: "배북 불테라 강챙이 샘플",
+      link: "assets/images/gce.png",
+      likeCount: 2620,
+      replyCount: 10,
+      created: DateTime.now()),
 ];
 
 class ListScreen extends StatefulWidget {
@@ -43,14 +79,22 @@ class _ListScreenState extends State<ListScreen> {
             childAspectRatio: 1.0,
           ),
           itemCount: poketmons.length,
-          itemBuilder: (_,int index) => GestureDetector(
-            // 포켓몬 상세 화면으로 이동
-            onTap: () {},
-            child: Image.network(
-              poketmons[index].link,
-              fit: BoxFit.cover,
-            ),
-          )),
+          itemBuilder: (_, int index) => GestureDetector(
+                // 포켓몬 상세 화면으로 이동
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(
+                        poketmon: poketmons[index],
+                      ),
+                    ),
+                  );
+                },
+                child: Image.asset(
+                  poketmons[index].link,
+                  fit: BoxFit.cover,
+                ),
+              )),
     );
   }
 }
